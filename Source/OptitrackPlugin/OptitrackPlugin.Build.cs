80086642d456227ea43c2099750f359b647f01a3
Florian Weidner <info@florianweidner.de>
2018-03-28 10:34:43 +0200

    Init.

---
+++ Source/OptitrackPlugin/OptitrackPlugin.Build.cs
@@ -0,0 +1,97 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.IO;
+
+public class OptitrackPlugin : ModuleRules
+{
+	public OptitrackPlugin(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+        
+        
+
+        PublicIncludePaths.AddRange(
+			new string[] {
+				"OptitrackPlugin/Public"
+				// ... add public include paths required here ...
+			}
+			);
+				
+		
+		PrivateIncludePaths.AddRange(
+			new string[] {
+				"OptitrackPlugin/Private",
+				// ... add other private include paths required here ...
+			}
+			);
+			
+		
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				"OptitrackPluginLibrary",
+                "Projects",
+				// ... add other public dependencies that you statically link with here ...
+			}
+			);
+			
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				// ... add private dependencies that you statically link with here ...	
+			}
+			);
+		
+		
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+				// ... add any modules that your module loads dynamically here ...
+			}
+			);
+
+        LoadNatNetLib(Target);
+	}
+
+    private void LoadNatNetLib(ReadOnlyTargetRules Target)
+    {
+        bool isLibrarySupported = false;
+        string LibrariesPath = "";
+
+        if (Target.Platform == UnrealTargetPlatform.Win64)
+        {
+            isLibrarySupported = true;
+            LibrariesPath = Path.Combine(ThirdPartyPath, "NatNetSDK", "lib", "x64");
+        }
+
+        if (isLibrarySupported)
+        {
+            // Include Libs
+            PublicLibraryPaths.Add(LibrariesPath);
+
+            PublicAdditionalLibraries.Add("NatNetLib.lib");
+
+            // Include path
+            PublicIncludePaths.Add(Path.Combine(ThirdPartyPath, "NatNetSDK", "include"));
+
+            // Delay-load the DLL, so we can load it from the right place first
+            PublicDelayLoadDLLs.Add("NatNetLib.dll");
+        }
+
+    }
+
+
+    // Get the full path the additional library to load
+    private string ThirdPartyPath
+    {
+        get { return Path.GetFullPath(Path.Combine(ModulePath, "../../ThirdParty/")); }
+    }
+    private string ModulePath
+    {
+        // get { return Path.GetDirectoryName(RulesCompiler.GetModuleFilename(this.GetType().Name)); }
+        get { return ModuleDirectory; }
+    }
+}
---
+++ Source/OptitrackPlugin/Public/OptitrackPlugin.h
@@ -0,0 +1,21 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "ModuleManager.h"
+
+
+
+
+class FOptitrackPluginModule : public IModuleInterface
+{
+public:
+
+	/** IModuleInterface implementation */
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+
+private:
+	/** Handle to the test dll we will load */
+	void*	NatNetHandle;
+};
