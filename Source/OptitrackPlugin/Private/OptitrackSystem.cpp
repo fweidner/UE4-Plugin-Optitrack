3bd0950fe8e7a638ba4e80ce5c519c07afdea3bc
Florian Weidner <info@florianweidner.de>
2018-04-10 12:57:08 +0200

    Added system class.

---
+++ Source/OptitrackPlugin/Private/OptitrackSystem.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "OptitrackSystem.h"
+
+OptitrackSystem::OptitrackSystem()
+{
+}
+
+OptitrackSystem::~OptitrackSystem()
+{
+}
